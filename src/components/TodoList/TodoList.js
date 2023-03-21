abefa5d831db761f987a3a69e8150603e1647c6c
sean <seanschmidt89@yahoo.com>
2023-03-20 22:45:01 -0700

    adding logic to todolist comp to save list

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,26 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import Todo from "../Todo/Todo";
 import { useSelector, useDispatch } from "react-redux";
 import { sortList } from "../../store/todoSlice";
 import "./TodoList.css";
 
 const TodoList = () => {
+  const [save, setSave] = useState(false);
+  const [inputText, setInputText] = useState("");
   const todos = useSelector((state) => state.todos.todos);
   const dispatch = useDispatch();
 
   const selectHandler = (e) => {
     dispatch(sortList(e.target.value));
   };
+
+  const showState = (e) => {
+    setSave(!save);
+  };
+
+  const saveListHandler = (e) => {
+    setInputText(e.target.value);
+  };
+
+  //need handler for button on save button
+  //then need to save list to slice
+
   return (
     <div className="todoList">
-      <div className="sort">
-        <p>Order by Priority</p>
-        <select onChange={selectHandler} defaultValue="-">
-          <option>-</option>
-          <option>High</option>
-          <option>Low</option>
-        </select>
-      </div>
+      {todos.length > 0 && (
+        <div className="sort">
+          <p>Order by Priority</p>
+          <select onChange={selectHandler} defaultValue="-">
+            <option>-</option>
+            <option>High</option>
+            <option>Low</option>
+          </select>
+          <div className="save-todo">
+            {save ? (
+              <button onClick={showState}>Cancel</button>
+            ) : (
+              <button onClick={showState}>Save List</button>
+            )}
+          </div>
+          {save && (
+            <div className="save-input">
+              <input
+                onChange={saveListHandler}
+                value={inputText}
+                placeholder="List Name"
+              />
+              <button>Save</button>
+            </div>
+          )}
+        </div>
+      )}
       <div className="list-container">
         {todos.length > 0 &&
           todos.map((item) => <Todo key={item.id} item={item} />)}
